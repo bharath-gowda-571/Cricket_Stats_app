695e5dd5988d9f7f4e6551ae53d6cc3fbb389abe
Bharath Gowda <bharath1234gowda@gmail.com>
2021-06-02 19:45:21 +0530

    june2

---
+++ lib/batsmanVsTeamSearch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BatsmanVsTeam extends StatefulWidget {
+  const BatsmanVsTeam({Key key}) : super(key: key);
+
+  @override
+  _BatsmanVsTeamState createState() => _BatsmanVsTeamState();
+}
+
+class _BatsmanVsTeamState extends State<BatsmanVsTeam> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
